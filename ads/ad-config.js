c0149de91065b75e24589dbd6eefcaa3a5e45974
Tharun9772Gaming <tharun2015gamer@gmail.com>
2025-11-21 12:57:35 -0500

    Update and rename bloxcraft-ubg-asset-1.json to ads/ad-config.js

---
+++ ads/ad-config.js
@@ -0,0 +1,6 @@
+// Ads Config
+// Updated By Bloxcraft Studios And Tharun9772Gaming When There Is Bugs
+// Do Not Modify Unless You Know What Your Doing
+// You May Use This For Your Own Sites
+// Ads Folder May Track IP Addresses, Unique Visitors, And Views Of Ads To Make Sure Ads Do NOT Repeat
+// The Owner May Not Have Accses To Them, Google Might Have Accses To Them Sadly.
